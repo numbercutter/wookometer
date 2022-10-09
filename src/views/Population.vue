b4fd3c5b7fa842d9d69542a354a3cca48fd92c5a
numbercutter <jamlets@protonmail.com>
2022-10-09 08:24:46 -0400

    wookometer

---
+++ src/views/Population.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>WOOK POPULATION</h1>
+    <h1>WOOK ECONOMICS</h1>
+    <h1>WOOK MEDIA</h1>
+    <h1>WOOK ENVIRONMENT</h1>
+    <h1>WOOK FOOD</h1>
+    <h1>WOOK WATER</h1>
+    <h1>WOOK ENERGY</h1>
+    <h1>WOOK HEALTH</h1>
+  </div>
+</template>
